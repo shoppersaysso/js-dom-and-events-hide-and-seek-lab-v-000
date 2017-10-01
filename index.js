9a5aa2752d71bd6f3ea0bdcd0dbc8df698783f7f
Sara Hopper <sara.hopper@gmail.com>
2017-10-01 19:17:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
 
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
 }
-
-#grand-node > div > div > div > div
